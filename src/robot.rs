f033fa4a8c7f665ee19ad7e6995ac70d62578b12
akhilveeraghanta <akhil.veeraghanta@outlook.com>
2022-11-25 15:40:26 -0800

    radio comms work

--- src/robot.rs
+++ src/robot.rs
@@ -3,18 +3,15 @@ extern crate panic_semihosting;
 use bxcan::Fifo;
 use cortex_m_semihosting::hprintln;
 use nb::block;
-use stm32f1xx_hal::device::adc2::jsqr::W;
 use stm32f1xx_hal::device::Peripherals;
 use stm32f1xx_hal::prelude::*;
-use stm32f1xx_hal::spi::{Mode, Phase, Polarity};
-use stm32f1xx_hal::{can, pac, prelude::*, spi};
+use stm32f1xx_hal::{can, spi};
 
-use embedded_nrf24l01::Configuration;
+use embedded_nrf24l01::{Configuration, Device};
 use embedded_nrf24l01::{CrcMode, DataRate, NRF24L01};
 
 pub fn run() {
     let device_peripherals = Peripherals::take().unwrap();
-    let mut cortex_peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
 
     let mut flash = device_peripherals.FLASH.constrain();
     let rcc = device_peripherals.RCC.constrain();
@@ -33,17 +30,14 @@ pub fn run() {
 
     let mut gpioa = device_peripherals.GPIOA.split();
     let mut gpiob = device_peripherals.GPIOB.split();
-    let mut gpioc = device_peripherals.GPIOC.split();
-
     let mut afio = device_peripherals.AFIO.constrain();
-
-    let mut ncs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
-    ncs.set_high();
 
     let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
     let miso = gpioa.pa6;
     let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
     let ce = gpioa.pa8.into_push_pull_output(&mut gpioa.crh);
+    let mut ncs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
+    ncs.set_high();
 
     let spi = spi::Spi::spi1(
         device_peripherals.SPI1,
@@ -53,28 +47,13 @@ pub fn run() {
             polarity: spi::Polarity::IdleLow,
             phase: spi::Phase::CaptureOnFirstTransition,
         },
-        10.MHz(),
+        8.MHz(),
         clocks,
     );
 
-    // nRF24L01 library specific starts here.
-    let mut nrf24 = NRF24L01::new(ce, ncs, spi).unwrap();
-
-    nrf24.set_frequency(10).unwrap();
-    nrf24.set_auto_retransmit(0, 0).unwrap();
-    nrf24.set_rf(&DataRate::R2Mbps, 3).unwrap();
-    nrf24
-        .set_pipes_rx_enable(&[true, false, false, false, false, false])
-        .unwrap();
-    nrf24.set_auto_ack(&[false; 6]).unwrap();
-    nrf24.set_crc(CrcMode::Disabled).unwrap();
-    nrf24.set_tx_addr(&b"fnord"[..]).unwrap();
-
     let can = can::Can::new(device_peripherals.CAN1, device_peripherals.USB);
-
     let rx = gpiob.pb8.into_floating_input(&mut gpiob.crh);
     let tx = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
-
     can.assign_pins((tx, rx), &mut afio.mapr);
 
     // APB1 (PCLK1): 8MHz, Bit rate: 125kBit/s, Sample Point 87.5%
@@ -93,11 +72,30 @@ pub fn run() {
     // Split the peripheral into transmitter and receiver parts.
     block!(can.enable_non_blocking()).unwrap();
 
+    let nrf24 = NRF24L01::new(ce, ncs, spi).unwrap();
+    let mut nrf = nrf24.tx().unwrap();
+    nrf.flush_tx().unwrap();
+    nrf.flush_rx().unwrap();
+    nrf.set_frequency(0x4c).unwrap();
+    nrf.set_auto_retransmit(0x0f, 0x0f).unwrap();
+    nrf.set_auto_ack(&[false; 6]).unwrap();
+    nrf.set_rf(&DataRate::R1Mbps, 3).unwrap();
+    nrf.set_crc(CrcMode::TwoBytes).unwrap();
+    nrf.set_tx_addr(&b"2Node"[..]).unwrap();
+
+    hprintln!("------");
+    hprintln!("ADDRWIDTH: {}", nrf.get_address_width().unwrap());
+    hprintln!("FREQ: {}", nrf.get_frequency().unwrap());
+    hprintln!("INTER: {}", nrf.get_interrupts().unwrap().0);
+    hprintln!("------");
+
     loop {
-        //if !nrf24.rx().unwrap().ready().is_err() {
-        //let mut buf = [0u8; 32];
-        //let len = nrf24.read(&mut buf).unwrap();
-        //hprintln!("Received: {:?}", buf);
-        //}
+        if nrf.can_send().unwrap()
+        {
+            nrf.send(&[1; 32]).unwrap(); 
+            hprintln!("Sent");
+        }
+        // wait for 1 second
+        cortex_m::asm::delay(48_000_000);
     }
 }
