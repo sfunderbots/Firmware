65a1523bcbaae5406c935ca6d87ce1f472a7d801
akhil <akhil.veeraghanta@outlook.com>
2023-05-30 20:56:30 -0700

    wheels spin

--- OpiSoftware/opi_main/src/main.rs
+++ OpiSoftware/opi_main/src/main.rs
@@ -15,22 +15,21 @@ fn main() -> anyhow::Result<()> {
     let mut tiny2 = Tinymovr::new(2, &mut sock);
     let mut tiny3 = Tinymovr::new(3, &mut sock);
 
-    println!("Calibration done");
+
+    println!("START CALIBRATION");
+    //tiny1.calibrate(&mut sock);
+    //tiny2.calibrate(&mut sock);
+    //tiny3.calibrate(&mut sock);
+
     tiny1.velocity_control(&mut sock);
-    println!("Velocity control done");
     tiny2.velocity_control(&mut sock);
-    println!("Velocity control done");
     tiny3.velocity_control(&mut sock);
-    println!("Velocity control done");
 
     // Sleep
-    std::thread::sleep(std::time::Duration::from_secs(1));
     tiny1.set_vel_integrator_params(0.02, 300.0, &mut sock);
     tiny2.set_vel_integrator_params(0.02, 300.0, &mut sock);
     tiny3.set_vel_integrator_params(0.02, 300.0, &mut sock);
-    std::thread::sleep(std::time::Duration::from_secs(1));
 
-    println!("Setting vel setpoint");
     tiny1.set_vel_setpoint(500000.0, &mut sock);
     tiny2.set_vel_setpoint(500000.0, &mut sock);
     tiny3.set_vel_setpoint(500000.0, &mut sock);
@@ -46,14 +45,4 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-fn frame_to_string<F: Frame>(frame: &F) -> String {
-    let id = frame.raw_id();
-    let data_string = frame
-        .data()
-        .iter()
-        .fold(String::from(""), |a, b| format!("{} {:02x}", a, b));
-
-    format!("{:X}  [{}] {}", id, frame.dlc(), data_string)
 }
