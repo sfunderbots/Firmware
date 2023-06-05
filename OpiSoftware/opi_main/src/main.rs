18cbbf1ac40580a06527f97cc23ad9320c48ca0e
akhil <akhil.veeraghanta@outlook.com>
2023-06-04 18:45:45 -0700

    wip

--- OpiSoftware/opi_main/src/main.rs
+++ OpiSoftware/opi_main/src/main.rs
@@ -2,44 +2,55 @@ mod tinymovr;
 mod wheel_conversion;
 use tinymovr::Tinymovr;
 use wheel_conversion::{LocalVelocity, WheelVelocity};
-use gilrs::{Gilrs, Button, Event};
 use anyhow::Context;
 use socketcan::{CanFrame, CanSocket, Frame, Socket};
 use std::env;
+use ftdi_embedded_hal as hal;
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
-    let mut sock = CanSocket::open("can0")
-        .with_context(|| format!("Failed to open socket on interface can0"))?;
-    println!("Socket opened");
+    
+    let device = libftd2xx::Ft2232h::with_description("Single RS232-HS A")?;
 
-    let mut tiny1 = Tinymovr::new(1, &mut sock);
-    let mut tiny2 = Tinymovr::new(2, &mut sock);
-    let mut tiny3 = Tinymovr::new(3, &mut sock);
+    //let mut sock = CanSocket::open("can0")
+        //.with_context(|| format!("Failed to open socket on interface can0"))?;
+    //let device = hal::find_by_vid_pid(0x0403, 0x6014)
+        //.interface(hal::Interface::A)
+        //.open()?;
+    
+    //let hal = hal::FtHal::init_freq(device, 3_000_000)?;
+    //let spi = hal.spi()?;
+    //let gpio = hal.ad6();
 
-    let mut gilrs = Gilrs::new().unwrap();
+    //println!("Socket opened");
 
-    // Iterate over all connected gamepads
-    for (_id, gamepad) in gilrs.gamepads() {
-        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
-    }
+    //let mut tiny1 = Tinymovr::new(1, &mut sock);
+    //let mut tiny2 = Tinymovr::new(2, &mut sock);
+    //let mut tiny3 = Tinymovr::new(3, &mut sock);
 
-    let mut active_gamepad = None;
+    //let mut gilrs = Gilrs::new().unwrap();
 
-    loop {
-        // Examine new events
-        while let Some(Event { id, event, time }) = gilrs.next_event() {
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            active_gamepad = Some(id);
-        }
+    //// Iterate over all connected gamepads
+    //for (_id, gamepad) in gilrs.gamepads() {
+        //println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    //}
 
-        // You can also use cached gamepad state
-        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
-            }
-        }
-    }
+    //let mut active_gamepad = None;
+
+    //loop {
+        //// Examine new events
+        //while let Some(Event { id, event, time }) = gilrs.next_event() {
+            //println!("{:?} New event from {}: {:?}", time, id, event);
+            //active_gamepad = Some(id);
+        //}
+
+        //// You can also use cached gamepad state
+        //if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+            //if gamepad.is_pressed(Button::South) {
+                //println!("Button South is pressed (XBox - A, PS - X)");
+            //}
+        //}
+    //}
 
     //println!("START CALIBRATION");
     //tiny1.calibrate(&mut sock);
